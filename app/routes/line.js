1c7dd9c59a4d88e91d5d84e45ef76f19b427ee8b
Pablo Ibanez <me@pabloibanez.com>
2018-09-29 23:03:16 +0100

    Line - Distance & year properties added

--- app/routes/line.js
+++ app/routes/line.js
@@ -13,6 +13,18 @@ module.exports = (app, modelsService) => {
     app.routesInfo['Line'].push({ model: 'Line', name: 'Get full info from line', method: 'GET', url: url });
   }
 
+  const registerCalculateLineDistance = () => {
+    const url = '/api/line/calculate-distance/:lineId';
+    app.get(url,
+      (req, res) => {
+        service.calculateLineDistance(modelsService, req.params.lineId)
+          .then(result => res.status(result.statusCode).send(result.data))
+          .catch(err => res.status(500).send(err));
+      });
+    app.routesInfo['Line'].push({ model: 'Line', name: 'Calculate line distance', method: 'GET', url: url });
+  }
+
   registerGetLineFullInfo();
+  registerCalculateLineDistance();
 
 };
--- app/services/line.service.js
+++ app/services/line.service.js
@@ -3,12 +3,24 @@ const service = {};
 service.getLineFullInfo = async (modelsService, lineId) => {
   const line = await modelsService.getModel('Line')
     .findById(lineId)
-    .select('name shortName colour fontColour startStations')
+    .select('name shortName colour fontColour startStations year distance')
     .populate({ path: 'connections', select: 'stations year yearEnd distance', populate: { path: 'stations', select: 'name' } })
   if (!line) {
     return { statusCode: 404, data: 'Line not found' };
   }
   return { statusCode: 200, data: { ...line._doc, connections: sortConnections(line._doc.connections, line._doc.startStations) } };
+}
+
+service.calculateLineDistance = async (modelsService, lineId) => {
+  const line = await modelsService.getModel('Line')
+    .findById(lineId)
+    .populate({ path: 'connections', select: 'distance' })
+  if (!line) {
+    return { statusCode: 404, data: 'Line not found' };
+  }
+  line.distance = line.connections.map(c => c.distance).reduce((prev, next) => { return prev + next });
+  await line.save();
+  return { statusCode: 202, data: `${line.name} total distance: ${line.distance}` };
 }
 
 const sortConnections = (connections, startStations) => {
